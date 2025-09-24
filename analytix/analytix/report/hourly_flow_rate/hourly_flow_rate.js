b9338234c5cf3b5c13b9eaa3d5f34b5cac7c837a
Sathish Kumar V <sathish@cognitionx.tech>
2025-09-24 18:01:13 +0530

    deleted hourly flow rate

--- analytix/analytix/report/hourly_flow_rate/hourly_flow_rate.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, CognitionX Logic India Private Limited and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Hourly Flow Rate"] = {
-	"filters": [
-
-	]
-};
--- analytix/analytix/report/hourly_flow_rate/hourly_flow_rate.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, CognitionX Logic India Private Limited and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-
-def execute(filters=None):
-	columns, data = [], []
-	return columns, data
