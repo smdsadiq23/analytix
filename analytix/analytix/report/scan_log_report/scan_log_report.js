5bff228b103abd2c57ed875a875a4106d9a51ca3
Sathish Kumar V <sathish@cognitionx.tech>
2025-10-06 16:57:11 +0530

    Update scan_log_report.js

--- analytix/analytix/report/scan_log_report/scan_log_report.js
+++ analytix/analytix/report/scan_log_report/scan_log_report.js
@@ -3,6 +3,31 @@
 
 frappe.query_reports["Scan Log Report"] = {
 	"filters": [
-
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "sales_order",
+			"label": __("Sales Order"),
+			"fieldtype": "Link",
+			"options": "Sales Order"
+		},
+		{
+			"fieldname": "work_order",
+			"label": __("Work Order"),
+			"fieldtype": "Link",
+			"options": "Work Order"
+		}
 	]
 };
