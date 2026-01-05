a42aed5044098567573d17b8b8daad81b968b6a4
Sathish Kumar V <sathish@cognitionx.tech>
2026-01-05 20:41:23 +0530

    Whatsapp Notification related doctypes

---
+++ analytix/analytix/doctype/whatsapp_notification/whatsapp_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, CognitionX Logic India Private Limited and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Whatsapp Notification", {
+// 	refresh(frm) {
+
+// 	},
+// });
