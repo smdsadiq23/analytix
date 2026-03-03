195ccdee7674e31676ac11de895d7fd53aedf400
Sathish Kumar V <sathish@cognitionx.tech>
2026-03-03 17:48:36 +0530

    Update cutting_balance_report.py

--- analytix/analytix/report/cutting_balance_report/cutting_balance_report.py
+++ analytix/analytix/report/cutting_balance_report/cutting_balance_report.py
@@ -38,6 +38,7 @@ def get_data():
         INNER JOIN `tabItem` item ON item.name = sod.item_code
         WHERE 
             so.docstatus = 1
+            AND so.custom_consumption_status - 'Inprogress'
             AND item.custom_select_master = 'Finished Goods'
             AND sod.name = (
                 SELECT sub_sod.name
